444ba9e12a71bb5dbdc6f1939f08691cde95d129
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-22 23:56:35 +0300

    create-reg-screen

---
+++ screens/RegistrationScreen.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, ImageBackground, Text } from "react-native";
+
+function RegistrationScreen() {
+  return (
+    <div>
+      <View>
+        <ImageBackground />
+      </View>
+    </div>
+  );
+}
+
+export default RegistrationScreen;
