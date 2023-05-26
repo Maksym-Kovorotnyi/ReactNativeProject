bf68dfecd7aa470def2abc8cc250fe9090de97ca
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-26 10:51:01 +0300

    Update RegistrationScreen.jsx

--- screens/RegistrationScreen.jsx
+++ screens/RegistrationScreen.jsx
@@ -6,6 +6,8 @@ import {
   StyleSheet,
   Image,
   TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
 } from "react-native";
 import {
   useFonts,
@@ -30,34 +32,33 @@ function RegistrationScreen() {
         source={require("../assets/images/bcg.png")}
         style={styles.bcgImage}
       >
-        <View style={styles.formContainer}>
-          <Image style={styles.userPhoto}></Image>
-          <Text
-            style={{
-              fontFamily: "Roboto_500Medium",
-              fontSize: 30,
-              marginBottom: 30,
-              textAlign: "center",
-            }}
-          >
-            Registration
-          </Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Login"
-            textContentType="nickname"
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Email address"
-            textContentType="emailAddress"
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Password"
-            textContentType="password"
-          />
-        </View>
+        <KeyboardAvoidingView behavior="height">
+          <View style={styles.formContainer}>
+            <Image style={styles.userPhoto}></Image>
+            <Text style={styles.title}>Registration</Text>
+            <View style>
+              <TextInput
+                style={styles.input}
+                placeholder="Login"
+                textContentType="nickname"
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Email address"
+                textContentType="emailAddress"
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                textContentType="password"
+              />
+            </View>
+            <TouchableOpacity activeOpacity={0.8} style={styles.btn}>
+              <Text style={styles.btnText}>Sign up</Text>
+            </TouchableOpacity>
+            <Text style={styles.text}>Already have an account? Log in</Text>
+          </View>
+        </KeyboardAvoidingView>
       </ImageBackground>
     </>
   );
@@ -69,35 +70,63 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     justifyContent: "flex-end",
   },
-  formContainer: {
-    flex: 0.6,
 
-    // alignItems: "center",
+  formContainer: {
     backgroundColor: "#fff",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
-    paddingTop: 92,
+    paddingBottom: 80,
+    marginTop: "auto",
   },
   userPhoto: {
     position: "absolute",
     alignSelf: "center",
-    top: "-10%",
+    top: -60,
     width: 120,
     height: 120,
     backgroundColor: "#F6F6F6",
     borderRadius: 16,
   },
+  title: {
+    fontFamily: "Roboto_500Medium",
+    fontSize: 30,
+    marginTop: 92,
+    marginBottom: 30,
+    textAlign: "center",
+  },
   input: {
     fontFamily: "Roboto_400Regular",
     fontSize: 16,
-    padding: 16,
-    marginBottom: 16,
     backgroundColor: "#F6F6F6",
     color: "#212121",
     borderWidth: 1,
     borderColor: "#E8E8E8",
     borderRadius: 8,
     marginHorizontal: 16,
+    padding: 16,
+    marginBottom: 16,
+  },
+  btn: {
+    backgroundColor: "#FF6C00",
+    marginTop: 17,
+    marginBottom: 16,
+    marginHorizontal: 16,
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 100,
+  },
+  btnText: {
+    color: "#fff",
+    fontFamily: "Roboto_400Regular",
+    fontSize: 16,
+    textAlign: "center",
+    lineHeight: 19,
+  },
+  text: {
+    fontFamily: "Roboto_400Regular",
+    fontSize: 16,
+    color: "#1B4371",
+    textAlign: "center",
   },
 });
 
